06f566cd1a7a7e212ecaac18e98c1204569b9512
Isabel Bush <ibush@stanford.edu>
2015-01-12 22:14:48 -0800

    Added rpi-install for Mac

---
+++ guides/mac_toolchain/rpi-install.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import sys
+import serial
+from xmodem import XMODEM
+
+if len(sys.argv) != 3:
+    print "usage: %s tty bin" % sys.argv[0]
+    sys.exit(1)
+
+portname = sys.argv[1]
+filename = sys.argv[2]
+
+port = serial.Serial(port=portname, baudrate=115200)
+if not port:
+    print portname, 'not found'
+    sys.exit(1)
+
+stream = file(filename, 'rb')
+if not stream:
+    print filename, 'not found'
+    sys.exit(1)
+
+# problem was the timeout=0
+#   needed to wait for NAK
+def getc(size, timeout=1):
+    return port.read(size)
+
+def putc(data, timeout=1):
+    port.write(data)
+
+xmodem = XMODEM(getc, putc)
+
+status = xmodem.send(stream)
+print 'sent', status
+
+stream.close()
+sys.exit(int(not status))
+
