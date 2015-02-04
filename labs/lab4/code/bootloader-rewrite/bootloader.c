feaa2a93a8f29dbc3d0988e0490e0ef32515cd15
dddrrreee <ddd.rrr.eee@gmail.com>
2015-02-03 23:00:06 -0800

    rewrite rewrite

--- labs/lab4/code/bootloader-rewrite/bootloader.c
+++ labs/lab4/code/bootloader-rewrite/bootloader.c
@@ -10,52 +10,35 @@
  */
 #include "rpi.h"
 
-#define ARMBASE 0x8000
-
 enum { 
-       SOH = 0x01,   // Start Of Header
-       ACK = 0x06,   // Acknowledge (positive)
-       NAK = 0x15,   // Acknowledge (negative)
-       EOT = 0x04,   // End of transmission 
-       PACKET_SIZE = 132
+       	SOH = 0x01,   // Start Of Header
+       	ACK = 0x06,   // Acknowledge (positive)
+       	NAK = 0x15,   // Acknowledge (negative)
+       	EOT = 0x04,   // End of transmission 
+
+       	PAYLOAD_SIZE = 128,
+	ARMBASE = 0x8000
 };
 
-struct serial_channel {
-    unsigned addr,
-    	     block,
-    	     state, // Stores which byte in block we're on
-	     crc,
-    	     rx;
-};
+static unsigned char serial_recv(void) { 
+	static unsigned rx;
+	if(!rx)
+		rx = timer_tick();
 
-static struct serial_channel 
-serial_start() {
-  	struct serial_channel s;
-	s.block = 1;
-    	s.addr = ARMBASE;
-    	s.state = 0;
-    	s.crc = 0;
-    	s.rx = timer_tick();
-	return s;
-}
-static unsigned char 
-serial_recv(struct serial_channel *s) {
 	// not ready, just keep going.
 	while(((uart_lcr() & 0x01) == 0)) {
 		// nak every 4 sec
-        	if ((timer_tick() - s->rx) >= 4000000) {
+        	if ((timer_tick() - rx) >= 4000000) {
             		uart_send(NAK);
-            		s->rx += 4000000;
+            		rx += 4000000;
         	}
 	}
         unsigned char uc = uart_recv();
-        s->rx = timer_tick();
+        rx = timer_tick();
 	return uc;
 }
 
 int notmain ( void ) {
-	struct serial_channel s;
-
 	uart_init();
     	hexstring(0x12345678);
     	hexstring(GETPC());
@@ -72,37 +55,33 @@ int notmain ( void ) {
      	 *  - CRC is over the whole packet 
      	 *  - after all packets sent, sender transmits a single EOT (must ACK).
      	 */
-	s = serial_start();
+	unsigned char block = 1, crc = 0;
+    	unsigned addr = ARMBASE;
     	while (1) {
 		unsigned char b, data[256];
 
         	// We received an EOT, send an ACK, jump to beginning of code
-        	if((b = serial_recv(&s)) == EOT) {
+        	if((b = serial_recv()) == EOT) {
               		uart_send(ACK);
               		BRANCHTO(ARMBASE);
 			return 0; // NOTREACHED
-		} else if(b == SOH) 
-                	s.crc = b;
-		else {
+		} else if(b != SOH) {
 			uart_send(NAK);  // Don't recognize command, NAK it
 			continue;	 // restart;
 		}
 
         	// Check if first byte of block is expected block number;
         	// if not, NAK, restart for next block
-		b = serial_recv(&s);
-        	if(b == s.block)
-        		s.crc += b;
-        	else {
+		if(serial_recv() != block) {
                 	uart_send(NAK);
 			continue;   // restart
 		}
 
 		// Check that second byte of block is negative of block
 		// number, if not, NAK, restart for next block
-		b = serial_recv(&s);
-        	if(b == (0xFF - s.block)) 
-			s.crc += b;
+		b = serial_recv();
+        	if(b == (0xFF - block)) 
+			crc = (SOH + block + b);
         	else {
 			uart_send(NAK);
 			continue;
@@ -110,21 +89,18 @@ int notmain ( void ) {
 	
 		// get the data bytes
 		int i;
-		for(i = 0; i < 128; i++)
-                	s.crc += (data[i] = serial_recv(&s));
+		for(i = 0; i < PAYLOAD_SIZE; i++)
+                	crc += (data[i] = serial_recv());
 	
-        	// At end of data block, check CRC/checksum (132 byte blocks)
-		s.crc &= 0xFF;
-
 		// Checksum failed: NAK the block
-		if(serial_recv(&s) != s.crc)
+		if(serial_recv() != crc)
 			uart_send(NAK);
 		// Checksum passed, copy block to program region.  ACK
 		else {
-               		for(i = 0; i < 128; i++)
-                        	PUT8(s.addr++, data[i]);
+               		for(i = 0; i < PAYLOAD_SIZE; i++)
+                        	PUT8(addr++, data[i]);
 			uart_send(ACK);
-			s.block = (s.block + 1) & 0xFF;
+			block++;
 		}
     	}
     	return(0);
