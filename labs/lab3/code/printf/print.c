88b0bdf70e9cf05fefb7eab7f14adc5ed1e5a7b5
Omar Rizwan <omar.rizwan@gmail.com>
2016-01-27 18:37:37 -0800

    [lab3] printf tweaks.

--- labs/lab3/code/printf/print.c
+++ labs/lab3/code/printf/print.c
@@ -1,29 +1,17 @@
-/* Not used until you call printf and gpio_set_function yourself. */
+/* Use the C standard headers so printf is defined. */
 #include <stdio.h>
-#include "gpio.h"
-
 #include "uart.h"
-#define PUTCHAR(C) uart_putc(C)
 #include "timer.h"
-#define DELAY(S) delay(S)
-
-void my_puts(char *s)
-{
-  while(*s)
-    PUTCHAR(*s++);
-
-  PUTCHAR('\n');
-}
 
 int main() {
 
   uart_init();
 
-  DELAY(10);
+  delay(10);
 
   while (1) {
-    my_puts("hello");
-    DELAY(1);
+    printf("hello\r\n");
+    delay(1);
   }
 
   return 0;
