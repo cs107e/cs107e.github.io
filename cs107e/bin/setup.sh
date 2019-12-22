5acb34ea15e3dd55b4486f2c864a5de2e1f695dd
Julie Zelenski <zelenski@cs.stanford.edu>
2019-12-22 07:24:09 +0000

    Edited magic setup script to deal with zsh also (now default on macos) commit 901ec76491f398c8268ed4c79e8c4ab136b539e1 Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Sat Dec 21 23:19:32 2019 -0800
        Edited magic setup script to deal with zsh also (now default on macos)

--- cs107e/bin/setup.sh
+++ cs107e/bin/setup.sh
@@ -1,9 +1,22 @@
-if [ ! -d "cs107e.github.io" ]; then
-    echo "This script should be run in your cs107e_home directory that contains the cs107e.github.io repository"
+# I am not particularly found of doing things behind student's 
+
+EXPECTED=~/cs107e_home/cs107e.github.io/cs107e
+if [ ! -d $EXPECTED ]; then
+    echo "Expected directory $EXPECTED. Not found. Exiting"
     exit 1
 fi
+case $SHELL in
+*/zsh) 
+   SHELL_CONFIG="$HOME/.zshrc"
+   ;;
+*/bash)
+   SHELL_CONFIG="$HOME/.bashrc"
+   ;;
+*)
+   echo "Sorry, cannot configure shell $SHELL"
+   exit 1
+esac
 
-echo export PATH=\"$PWD/cs107e.github.io/cs107e/bin:\$PATH\" | tee -a ~/.bashrc ~/.bash_profile
-echo export CS107E=\"$PWD/cs107e.github.io/cs107e\" | tee -a ~/.bashrc ~/.bash_profile
-
-source ~/.bashrc
+echo "# Below two lines added by cs107e automated setup script" >> $SHELL_CONFIG
+echo "export CS107E=$EXPECTED" >> $SHELL_CONFIG
+echo "export PATH=\$PATH:\$CS107E/bin" >> $SHELL_CONFIG
