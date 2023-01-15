0588c4ac0a9a550d132c43655c9574c083cc7f1b
My Name <myemail@stanford.edu>
2023-01-15 20:39:05 +0000

    My version of on. commit 1b1b9c21ae6ed4e8289c86b313afc54836dc2fde Author: My Name <myemail@stanford.edu> Date:   Sun Jan 15 12:33:44 2023 -0800
        My version of on.

--- lectures/ARM/code/on/on.s
+++ lectures/ARM/code/on/on.s
@@ -1,14 +1,15 @@
-// turn on LED connected to GPIO20
+q// turn on LED connected to GPIO20
 
 // configure GPIO 20 for output
 //
 // GPIO 20 function select is in the 2nd function select register
 //
 
-# FSEL2= 0x20200008
-mov r0, #0x20000000
-orr r0, #0x00200000
-orr r0, #0x00000008
+mov r0, #0x20      // #0x00000020
+lsl r1, r0, #24    // #0x20000000
+lsl r2, r0, #16    // #0x00200000
+orr r0, r1, r2     // #0x20200000
+orr r0, r0, #0x08  // #0x20200008
 mov r1, #1         // indicates OUTPUT
 str r1, [r0]       // store 1 to address 0x20200008
 
