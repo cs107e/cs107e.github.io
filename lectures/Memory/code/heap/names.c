8b2ba68178fab23d437e932334dfc3a47d1f359a
Julie Zelenski <zelenski@cs.stanford.edu>
2023-02-09 02:31:05 +0000

    fix typo in comment commit 995a44dd5924b5f3f441406c7bf63470313d6f01 Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Wed Feb 8 18:18:21 2023 -0800
        fix typo in comment

--- lectures/Memory/code/heap/names.c
+++ lectures/Memory/code/heap/names.c
@@ -1,31 +1,22 @@
 #include "printf.h"
+#include "read.h"
 #include "strings.h"
 #include "uart.h"
 
 #define MAX_LINE_LEN 64
 
-static int get_and_echo(void)
-{
-    char ch = uart_getchar();   // read input from terminal
-    return uart_putchar(ch);    // echo char to terminal
-}
-
-static char *read_line(void)
-{
+static char *read_line(void) {
     char buf[MAX_LINE_LEN];
     char *result = buf; // what memory address does result point to?
 
     for (int i = 0; i < MAX_LINE_LEN - 1; i++) {
-        char ch = get_and_echo();
-        result[i] = ch == '\n' ? '\0' : ch;
+        result[i] = read_char();
         if (!result[i]) break;
     }
     return result;
 }
 
-
-void main(void)
-{
+void main(void) {
     uart_init();
 
     int n = 3;
@@ -34,8 +25,7 @@ void main(void)
     printf("\nEnter %d names, one per line\n", n);
     for (int i = 0; i < n; i++) {
         printf("Name: ");
-        char *line = read_line();
-        names[i] = line;
+        names[i] = read_line();
     }
     printf("Read %d names.\n", n);
 
