ead5d160b65eba6454b102c62441d59c4b7452b9
Julie Zelenski <zelenski@cs.stanford.edu>
2024-01-17 22:16:40 +0000

    Use Daniel's nifty update of pinout.py! commit 3a50ec4ebd5279351d9135ea6fc4678f5d15fb5d Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Wed Jan 17 14:14:21 2024 -0800
        Use Daniel's nifty update of pinout.py!

--- cs107e/bin/pinout.py
+++ cs107e/bin/pinout.py
@@ -2,30 +2,44 @@
 """
 Quick & dirty python script to print ASCII art version of Mango Pi pinout
 
-Author: Julie Zelenski <zelenski@cs.stanford.edu>
-Updated: Sat Dec 30 13:21:15 PST 2023
+Author: Julie Zelenski <zelenski@cs.stanford.edu> Dec 2023
+Updated by Daniel James <drjames@stanford.edu> Jan 2024
 """
 import sys
 
 USE_COLORS = True
 def wrap(text, *colors):
-    COLORS = {'normal':0, 'black':40,'red':41,'green':42,'yellow':43,'blue':44,'magenta':35, 'whitefg':37}
+    COLORS = {
+        'normal': 0,
+        'black': 100,
+        'black_fg': 90,
+        'red': 41,
+        'red_fg': 31,
+        'green': 42,
+        'green_fg': 32,
+        'yellow': 43,
+        'yellow_fg': 33,
+        'blue': 44,
+        'blue_fg': 34,
+        'magenta_fg': 35,
+        'white_fg': 97
+        }
     ansi_escape = lambda name: f"\033[{COLORS[name]}m" if USE_COLORS else ''
     return ''.join((ansi_escape(c) for c in colors)) + text + ansi_escape('normal')
 
 BOARD = [
     "                                             ",
-    "    |OTG|  |USB|                | HDMI |     ",
-    " O--|   |--|   |----------------| mini |---O ",
-    " |                  +-------+      +-----+ | ",
-    " |     " + wrap('Mango Pi','magenta') + "     |  D1   |      |micro| | ",
-    " |     " + wrap(' MQ-Pro ','magenta') + "     |  SoC  |      | sd  | | ",
-    " |                  +-------+      +-----+ | ",
-    " |                                         | ",
-    " | @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ 1 | ",
-    " | - - - - - - - - - - - - - - - - - - - - | ",
-    " | @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ | ",
-    " O-----------------------------------------O ",
+    "    │ OTG │  │ USB │            │ HDMI │     ",
+    " O──┤     ├──┤     ├────────────┤ mini ├───O ",
+    " │                  ┌──────┐      ┌─────┐  │ ",
+    " │     " + wrap('Mango Pi','magenta_fg') + "     │  D1  │      │ µsd │  │ ",
+    " │     " + wrap(' MQ-Pro ','magenta_fg') + "     │      │      │     │  │ ",
+    " │                  └──────┘      └─────┘  │ ",
+    " │                                         │ ",
+    " │ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ │ ",
+    " │ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ │ ",
+    " │ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ │ ",
+    " O─────────────────────────────────────────O ",
 ]
 
 HEADERS = [
@@ -52,7 +66,7 @@ HEADERS = [
 ]
 
 def colored_header(str, colors):
-    return ''.join((wrap(' ', next(colors)) if ch == '@' else ch) for ch in str)
+    return ''.join((wrap(' ', next(colors))) if ch == '@' else ch for ch in str)
 
 def print_board():
     indexes = iter(range(0, 2))
@@ -64,15 +78,14 @@ def print_board():
 
 def print_header_table():
     pin_numbers = iter(range(1, 41))
-    pin = lambda color: wrap(f"{next(pin_numbers):2}",color,'whitefg')
+    pin = lambda color: wrap("▐", color+"_fg") + wrap(f"{next(pin_numbers):2}","white_fg", color) + wrap("▌", color + "_fg")
     for pair in HEADERS:
         l_label,l_pin = pair[0][0], pin(pair[0][1])
         r_label,r_pin = pair[1][0], pin(pair[1][1])
-        print(f"  {l_label:6} {l_pin}|{r_pin}    {r_label}")
+        print(f"  {l_label:4} {l_pin} │ {r_pin} {r_label}")
 
 if __name__ == '__main__':
     USE_COLORS = not(len(sys.argv) >= 2 and sys.argv[1].startswith('-n'))
-    print()
     print_board()
     print()
     print_header_table()
