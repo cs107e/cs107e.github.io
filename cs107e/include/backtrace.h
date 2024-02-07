eeab1a7a07f92ec4897eae0e20e35e64b3efd916
Julie Zelenski <zelenski@cs.stanford.edu>
2024-02-07 01:45:06 +0000

    update cs107e public commit 87bc44454ed18caf7acb63940a886551d6225e88 Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Tue Feb 6 17:43:03 2024 -0800
        update cs107e public

--- cs107e/include/backtrace.h
+++ cs107e/include/backtrace.h
@@ -14,95 +14,70 @@
 /*
  * Type: `frame_t`
  *
- * This struct stores the information for a function who has a frame on
- * the current stack.
- *
- * The `name` field is the name of the function as found by `name_of`.
- *
- * The `resume_addr` field is taken from saved lr in the callee. The saved lr
- * (sometimes referred to as "return address") is the address of the
- * instruction in the caller's sequence of instructions where control will
- * resume after the callee returns.  The `uintptr_t` type is an unsigned integer
- * of the appropriate size to store an address. This type is used to hold
- * an address that you intend to treat numerically.
- *
- * The `resume_offset` is the number of bytes from the start of the function to
- * the `resume_addr`. This offset represents how far control has advanced into
- * the caller function before it invoked the callee.
+ * This struct stores the information for a function that has a frame on the
+ * call stack. The `resume_addr` is taken from saved ra in the callee frame.
+ * The return address in the caller's sequence of instructions where control
+ * will resume after the callee returns. The type `uintptr_t` is an
+ * unsigned int/long of the appropriate bitwidth to store an address. The
+ * `uintptr_t` type is used for an address that you intend to treat numerically.
  */
 typedef struct {
-    const char *name;
     uintptr_t resume_addr;
-    int resume_offset;
 } frame_t;
 
 /*
- * `backtrace`
+ * `backtrace_gather_frames`
  *
- * Gathers a backtrace for the calling program and writes the
- * call stack information into the array pointed to by `f`.
+ * Gathers a backtrace by harvesting frames from the current call stack and
+ * storing into the frame array `f`.
  *
  * A backtrace is the sequence of currently active function calls. Each element
  * in the array `f` is of type `frame_t` (struct described above). Each struct
  * represents a caller who has a frame on the stack.
  *
- * The `max_frames` argument specifies the maximum number of frames that can be
- * stored in the array f.  If the backtrace extends more than `max_frames`,
- * only the `max_frames` most recent calls are stored into the array.
+ * The `max_frames` argument specifies the maximum number of frames to harvest.
+ * If the backtrace extends more than `max_frames`, only the `max_frames`
+ * topmost calls are stored into the array.
  *
- * The return value of the function is the count of frames written to `f`.
+ * The function returns the count of frames written to `f`.
  *
  * @param f            array in which to write stack frames
  * @param max_frames   maximum number of frames that can be stored in array
  * @return             count of frames written to array
  */
-int backtrace(frame_t f[], int max_frames);
+int backtrace_gather_frames(frame_t f[], int max_frames);
 
 /*
- * `print_frames`
+ * `backtrace_print_frames`
  *
- * Given an array of frames that has previously been filled in by a call to
- * backtrace(), `print_frames` prints the backtrace information, one line per
- * frame, using this format:
+ * Given an array of frames as filled in by a call to backtrace_gather_frames(),
+ * this function prints the backtrace,  one line per frame, using the format:
  *
- *     #0 0x85f8 at malloc+132
- *     #1 0x8868 at strndup+28
- *     #2 0x8784 at main+24
+ *      #1  0x40000124
+ *      #2  0x400001d8
+ *      #3  0x4000004c
+ *      #4  0x40000010
+ *
+ * If symbols are available, each frame is augmented with its label.
+ * The label is the name of the function and offset for resume_addr.
+ * The offset is the number of bytes from function start to resume_addr.
+ * This offset represents how far control has advanced into the caller
+ * function before it invoked the callee. The label information is added
+ * to the end of the line using this format:
+ *
+ *     #2 0x400001d8 at main+28
  *
  * @param f     array of stack frames
  * @param n     number of frames in array
  */
-void print_frames(frame_t f[], int n);
+void backtrace_print_frames(frame_t f[], int n);
 
 /*
- * `print_backtrace`
+ * `backtrace_print`
  *
- * Convenience function that calls `backtrace` and `print_frames` to display
- * stack frames of currently executing program.
+ * Convenience function that calls `backtrace_gather_frames` and
+ * `backtrace_print_frames` to display current call stack.
  */
-void print_backtrace(void);
-
-/*
- * `name_of`
- *
- * The argument to `name_of` is the numeric address in memory of the first
- * instruction of a function and `name_of` returns the name of that
- * function.
- *
- * When compiling with the `-mpoke-function-name` flag, each function's
- * name is written into the text section alongside its instructions.
- * `name_of` finds a function's name by looking in the appropriate
- * location relative to the function's start address. The `uintptr_t` type
- * is an unsigned integer of the appropriate size to store an address.
- * This type is used to hold an address that you intend to treat numerically.
- *
- * If no name is available for the given address, `name_of` returns
- * the constant string "???"
- *
- * @param fn_start_addr     numeric address of instruction in memory
- * @return                  pointer to string of function's name or
- *                          constant string "???" if name not available
- */
-const char *name_of(uintptr_t fn_start_addr);
+void backtrace_print(void);
 
 #endif
--- cs107e/include/disassemble.h
+++ cs107e/include/disassemble.h
@@ -5,13 +5,15 @@
  * Functions to operate on text section:
  *    - decode RISC-V instruction into a string
  *      (used by printf %pI format)
- *    - translate address to symbolic name and vice versa
+ *    - translate address to symbolic name
+ *      (used by printf %pL format)
  *
  * Author: Julie Zelenski <zelenski@cs.stanford.edu>
  */
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 /*
  * `disassemble_insn`
@@ -24,7 +26,7 @@
  * @param addr      memory location of instruction to decode
  * @return          size in bytes of decoded instruction (4 or 2 if compressed)
  */
-int disassemble_insn(char *buf, size_t bufsize, const void *addr);
+int disassemble_insn(char *buf, size_t bufsize, uintptr_t addr);
 
 /*
  * `disassemble_label`
@@ -37,42 +39,17 @@ int disassemble_insn(char *buf, size_t bufsize, const void *addr);
  * @param bufsize   size of destination buffer (output truncated to fit if needed)
  * @param addr      memory location of instruction
  */
-void disassemble_label(char *buf, size_t bufsize, const void *addr);
-
-/*
- * `disassemble_name_to_addr`
- *
- * Searches text section to find a function with embedded name that
- * matches `name`.
- *
- * @param name      name of function to search for
- * @return          address of function's first instruction, NULL if not found
- */
-const void *disassemble_name_to_addr(const char *name);
-
-/*
- * `disassemble_addr_to_name`
- *
- * Searches backwards from `addr` to find embedded name for the
- * function containing instruction at `addr`. If `p_offset` is non-NULL,
- *`*p_offset` is assigned the count of bytes bewteen `addr` and
- * the function's first instruction.
- *
- * @param addr      address of instruction
- * @param p_offset  output parameter, *p_offset = number of bytes into function
- * @return          function's name, NULL if not found
- */
-const char *disassemble_addr_to_name(const void *addr, int *p_offset);
+void disassemble_label(char *buf, size_t bufsize, uintptr_t addr);
 
 /*
  * `disassemble_is_valid_insn`
  *
- * Return num bytes in insn if valid. An insn ddress is valid if it lies within
+ * Return num bytes in insn if valid. An insn address is valid if it lies within
  * the text section and contents at addr can be decoded as an instruction.
  *
  * @param addr      address of instruction
  * @return          2 or 4 if addr is valid text, 0 otherwise
  */
-int disassemble_is_valid_insn(const void *addr);
+int disassemble_is_valid_insn(uintptr_t addr);
 
 #endif
