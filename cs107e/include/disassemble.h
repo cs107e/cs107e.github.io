bea9b11e0eb3688e9095065223c62e4246c548e5
Julie Zelenski <zelenski@cs.stanford.edu>
2024-02-08 00:57:33 +0000

    libmango refactoring commit 8ae6057ccd8dd91508d995375ffe403e25dcb391 Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Wed Feb 7 16:48:11 2024 -0800
        libmango refactoring

--- cs107e/include/disassemble.h
+++ cs107e/include/disassemble.h
@@ -5,8 +5,6 @@
  * Functions to operate on text section:
  *    - decode RISC-V instruction into a string
  *      (used by printf %pI format)
- *    - translate address to symbolic name
- *      (used by printf %pL format)
  *
  * Author: Julie Zelenski <zelenski@cs.stanford.edu>
  */
@@ -27,19 +25,6 @@
  * @return          size in bytes of decoded instruction (4 or 2 if compressed)
  */
 int disassemble_insn(char *buf, size_t bufsize, uintptr_t addr);
-
-/*
- * `disassemble_label`
- *
- * Determines label for instruction at address and writes label
- * to `buf`, truncated to  `bufsize`. Label is in the form
- * "<function+offset>" if function name available, otherwise "<>"
- *
- * @param buf       destination buffer where to write label
- * @param bufsize   size of destination buffer (output truncated to fit if needed)
- * @param addr      memory location of instruction
- */
-void disassemble_label(char *buf, size_t bufsize, uintptr_t addr);
 
 /*
  * `disassemble_is_valid_insn`
--- cs107e/include/symtab.h
+++ cs107e/include/symtab.h
@@ -2,9 +2,10 @@
 #define SYMTAB_H
 
 /*
- * Functions to operate on symtab data (if available):
+ * Functions to operate on symbol table (if available):
  *    - find symbol for name
  *    - find symbol for address
+ *    - label for address (used by printf %pL format)
  *
  * Author: Julie Zelenski <zelenski@cs.stanford.edu>
  */
@@ -14,31 +15,46 @@
 #include <stdint.h>
 
 typedef struct {
-    const char *name;
-    uintptr_t addr; // start address of symbol
-    size_t size;    // size in bytes
+    const char *name;   // symbol name
+    uintptr_t addr;     // symbol start address
+    size_t size;        // symbol size in bytes
 } symbol_t;
 
 /*
- * `symtab_find_name`
+ * `symtab_symbol_for_name`
  *
- * Find symbol for `name`, store symbol information into
- * symbol_t reference parameter.
+ * Search for a symbol matching `name`. If match found, store
+ * symbol data into parameter `*p_symbol`.
  *
  * @param name      name of function to search for
  * @return          true if found, false otherwise
  */
-bool symtab_find_name(const char *name, symbol_t *p_symbol);
+bool symtab_symbol_for_name(const char *name, symbol_t *p_symbol);
 
 /*
- * `symtab_addr_to_name`
+ * `symtab_symbol_for_addr`
  *
- * Find symbol for `addr`, store symbol information into
- * symbol_t reference parameter.
+ * Search for a symbol that contains `addr`. If match found, store
+ * symbol data into parameter `*p_symbol`.
  *
- * @param addr      address of instruction
+ * @param addr      address to search for
  * @return          true if found, false otherwise
  */
-bool symtab_find_addr(uintptr_t addr, symbol_t *p_symbol);
+bool symtab_symbol_for_addr(uintptr_t addr, symbol_t *p_symbol);
+
+/*
+ * `symtab_label_for_addr`
+ *
+ * Determines label for instruction at address and write label
+ * to `buf`, truncated to `bufsize`. If the symbol name is available,
+ * label is of "<symbol_name+offset>". Without symbol names, the
+ * basic label is "<.text+offset>" for an address within the text
+ * section and "<>" otherwise.
+ *
+ * @param buf       destination buffer where to write label
+ * @param bufsize   size of destination buffer (label truncated to fit if needed)
+ * @param addr      addr to provide label for
+ */
+void symtab_label_for_addr(char *buf, size_t bufsize, uintptr_t addr);
 
 #endif
