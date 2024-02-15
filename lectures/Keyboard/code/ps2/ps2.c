64f5f8eb83ff11eb839550c4cb2941a03c45e6c7
Julie Zelenski <zelenski@cs.stanford.edu>
2024-02-15 17:54:47 +0000

    small code tweak for lecture ps2 example commit 71d53d51c836d7520f449a708f1b042a71c0b587 Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Thu Feb 15 09:50:46 2024 -0800
        small code tweak for lecture ps2 example

--- lectures/Keyboard/code/ps2/ps2.c
+++ lectures/Keyboard/code/ps2/ps2.c
@@ -1,34 +1,35 @@
 #include "gpio.h"
 #include "gpio_extra.h"
+#include "keyboard.h"
 #include "uart.h"
 
-static const gpio_id_t CLK = GPIO_PG12;
-static const gpio_id_t DATA = GPIO_PB7;
 
-static void wait_for_falling_clock_edge(void) {
-    while (gpio_read(CLK) == 0) {}
-    while (gpio_read(CLK) == 1) {}
+static void wait_for_falling_clock_edge(gpio_id_t clock) {
+    while (gpio_read(clock) != 1) {} // wait until clock is high
+    while (gpio_read(clock) == 1) {} // wait until clock is low
 }
 
-static int read_bit(void) {
-    wait_for_falling_clock_edge();
-    // read DATA when clock is low
-    return gpio_read(DATA);
+static int read_bit(gpio_id_t clock, gpio_id_t data) {
+    wait_for_falling_clock_edge(clock);
+    return gpio_read(data);
 }
 
 void main(void) {
+    gpio_id_t clock = KEYBOARD_CLOCK; // which gpios are these? read keyboard.h to find out!
+    gpio_id_t data = KEYBOARD_DATA;
+
     gpio_init();
     uart_init();
 
-    gpio_set_input(CLK);
-    gpio_set_pullup(CLK); // From gpio_extra.h
+    gpio_set_input(clock);
+    gpio_set_pullup(clock); // From gpio_extra.h
 
-    gpio_set_input(DATA);
-    gpio_set_pullup(DATA);
+    gpio_set_input(data);
+    gpio_set_pullup(data);
 
     while (1) {
         for (int i = 0; i < 11; i++) {
-            int bit = read_bit();
+            int bit = read_bit(clock, data);
             uart_putchar(bit + '0');
         }
         uart_putchar('\n');
