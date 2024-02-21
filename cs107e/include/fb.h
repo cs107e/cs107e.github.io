1ee3783064bd30cd39a71ad1c893b5d59ff7e92d
Julie Zelenski <zelenski@cs.stanford.edu>
2024-02-21 02:14:57 +0000

    assign6 wip commit 3494a34b19f5aaf5bde1e5cfd48a858610afee99 Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Tue Feb 20 18:12:33 2024 -0800
        assign6 wip

--- cs107e/include/fb.h
+++ cs107e/include/fb.h
@@ -27,9 +27,9 @@ typedef enum { FB_SINGLEBUFFER = 0, FB_DOUBLEBUFFER = 1 } fb_mode_t;
  * The depth of the framebuffer is always 4 bytes; ech pixel is
  * 32-bit value in format BGRA.
  *
- * If the requested size can be successfully accommodated, the
- * function returns normally. Otherwise on error, the function raises
- * a failed assert.
+ * If the requested size can be successfully accommodated by the underlying
+ * hardware, the function returns normally. Otherwise an assert will be raised
+ * within de/hdmi to report the problem.
  */
 void fb_init(int width, int height, fb_mode_t mode);
 
