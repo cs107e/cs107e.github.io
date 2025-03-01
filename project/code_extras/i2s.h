c553a62907b3e9de7b577fdb58aca1bc61e7f25c
Julie Zelenski <zelenski@users.noreply.github.com>
2025-03-01 04:06:54 +0000

    code extras commit 41ddea6d517922b35a956b5a7da1fdbaa1e055ca Author: Julie Zelenski <zelenski@users.noreply.github.com> Date:   Fri Feb 28 23:04:24 2025 -0500
        code extras

--- project/code_extras/i2s.h
+++ project/code_extras/i2s.h
@@ -1,1 +1,23 @@
-../../private/libraries/libextra/i2s.h
+#ifndef _I2S_H
+#define _I2S_H
+
+/*
+ * Hardware abstractions for pulse width modulation (I2S) of
+ * digital signals.
+ *
+ * Author: Chris Gregg <cgregg@stanford.edu>
+ * Author: Julie Zelenski <zelenski@cs.stanford.edu>
+ */
+
+#include <stdint.h>
+
+typedef enum {
+    I2S_MONO = 0,
+    I2S_STEREO = 1,
+} i2s_frame_type_t;
+
+void i2s_init(int sample_frequency);
+
+void i2s_play_stream(int sample_freq, const int16_t data[], int ndata, i2s_frame_type_t ftype);
+
+#endif
